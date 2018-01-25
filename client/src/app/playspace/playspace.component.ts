5179dc2e6100b1e7ada7322a8a52ef17cb54a099
Dave Carlisle <dave.carlisle@gmail.com>
2018-01-24 20:08:30 -0600

    working button and hand array

--- client/src/app/playspace/playspace.component.ts
+++ client/src/app/playspace/playspace.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from './shared/model/card';
+
 
 @Component({
   selector: 'tcc-playspace',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayspaceComponent implements OnInit {
 
+  hand: Card[] = [];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onDealClick() {
+    this.hand.push(
+      {
+        suit: "Spades",
+        rank: "Ace"
+      });
   }
 
 }
---
+++ client/src/app/playspace/shared/model/card.ts
@@ -0,0 +1,4 @@
+export interface Card {
+suit: string,
+rank: string
+}
