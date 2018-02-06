b19caa81178ab124c9b445ddfb02f3b55397547f
Dave Carlisle <dave.carlisle@gmail.com>
2018-02-06 08:08:19 -0600

    remove unused scrollglue stuff

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material/material.module';
-import { ScrollGlueDirective } from './scroll-glue.directive';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule
   ],
-  exports: [ 
-    MaterialModule 
-  ],
-  declarations: [ScrollGlueDirective]
+  exports: [
+    MaterialModule
+  ]
 })
 export class SharedModule { }
