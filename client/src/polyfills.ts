c9fe72c7d6bc526246870426d45e12a025ebc66b
Dave Carlisle <dave.carlisle@gmail.com>
2018-08-22 15:43:47 -0500

    Update to Angular 6, fix vulnerable dependencies, general package update

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -54,6 +54,7 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
  * Zone JS is required by Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
+(window as any).global = window; // resolve socket.io dependency on node global
 
 
 
