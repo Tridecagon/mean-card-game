b4fd9429e23dbd6abfb245ffce7ad826ad978370
Dave Carlisle <dave.carlisle@monsanto.com>
2019-04-16 07:32:48 -0700

    refactor cardrow into separate component

---
+++ client/src/app/tablespace/playspace/cardrow/cardrow.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardrowComponent } from './cardrow.component';
+
+describe('CardrowComponent', () => {
+  let component: CardrowComponent;
+  let fixture: ComponentFixture<CardrowComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CardrowComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CardrowComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/tablespace/playspace/cardrow/cardrow.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { UiCard } from 'app/shared/model';
+import { trigger, transition, style, animate, state } from '@angular/animations';
+
+@Component({
+  selector: 'mcg-cardrow',
+  templateUrl: './cardrow.component.html',
+  styleUrls: ['./cardrow.component.css'],
+  animations: [
+    trigger('dealCard', [
+      transition(
+        ':enter', [
+          style({ top: '35%', left: '42%' }),
+          animate(300, style({ top: '*', left: '*' }))
+        ])
+    ]),
+    trigger('flipCard', [
+      state('down', style({ transform: 'rotateY(180deg)' })),
+      transition('up <=> down', animate(300))
+    ])
+  ]
+})
+export class CardrowComponent implements OnInit {
+
+  @Input() cards: UiCard[];
+  @Output() onClick = new EventEmitter<{card: UiCard, index: number}>();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  computeLeft(card, i): number {
+    card.leftPos = (i * 3.3) + ((26 - this.cards.length) * 1.65) + .1;
+    return card.leftPos;
+  }
+
+  clicked(card: UiCard, index: number) {
+    console.log("Cardrow click handler");
+    this.onClick.emit({card, index});
+  }
+}
