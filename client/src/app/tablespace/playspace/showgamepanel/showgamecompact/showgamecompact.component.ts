fb84a625feba04328e92a9c9f6833cc92f744835
Dave Carlisle <dave.carlisle@gmail.com>
2020-04-12 21:27:37 -0500

    fix flipcard animation

--- client/src/app/tablespace/playspace/showgamepanel/showgamecompact/showgamecompact.component.ts
+++ client/src/app/tablespace/playspace/showgamepanel/showgamecompact/showgamecompact.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { ShowgamepanelComponent } from '../showgamepanel.component';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'mcg-showgamecompact',
   templateUrl: './showgamecompact.component.html',
-  styleUrls: ['./showgamecompact.component.css']
+  styleUrls: ['./showgamecompact.component.css'],
+  animations: [
+    trigger('flipCard', [
+      state('down', style({ transform: 'rotateY(180deg)' })),
+      transition('up <=> down', animate(300))
+    ])
+  ]
 })
 export class ShowgamecompactComponent extends ShowgamepanelComponent {
 
