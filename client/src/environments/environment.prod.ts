6e63431bdbd8ea1eab524f585c3ad0f76a77f395
Dave Carlisle <dave.carlisle@gmail.com>
2023-02-25 12:39:57 -0600

    fix url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   old_server_url: 'https://mean-card-game-server.herokuapp.com',
-  server_url: 'https://skat-server.up-railway.app'
+  server_url: 'https://skat-server.up.railway.app'
 };
