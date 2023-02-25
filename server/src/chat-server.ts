28d2e4ffd8af4dd5cb8e4da60c6021bb37fd104d
Dave Carlisle <dave.carlisle@gmail.com>
2023-02-25 12:45:44 -0600

    add client to cors whitelist

--- server/src/chat-server.ts
+++ server/src/chat-server.ts
@@ -54,7 +54,7 @@ export class ChatServer {
             this.socketOpts.pingTimeout = 300000;
         }
         this.socketOpts.cors = {
-            origin: "http://localhost:4200",
+            origin: ["http://localhost:4200", "https://skat.up.railway.app:*"]
           };
     }
 
