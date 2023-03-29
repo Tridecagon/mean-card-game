f6bb64e5ff6487c83f6d0bec3664c88a7dd10f7d
Dave Carlisle <dave.carlisle@gmail.com>
2023-03-28 23:21:56 -0500

    fix fetch

--- client/server.js
+++ client/server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const fetch = require('node-fetch');
 
 const skatRules = fs.readFileSync('./src/assets/skatrules.md').toString();
 
